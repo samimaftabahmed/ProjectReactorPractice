3f20afee43d813b1a25cbd248a029db5d8f6791d
Samim <samimaftabahmed@gmail.com>
2022-10-22 01:52:24 +0530

    Random userid

--- src/main/java/com/samim/reactor/monolesson/MonoEmptyOrError.java
+++ src/main/java/com/samim/reactor/monolesson/MonoEmptyOrError.java
@@ -3,10 +3,13 @@ package com.samim.reactor.monolesson;
 import com.samim.reactor.courseutil.Util;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MonoEmptyOrError {
 
     public static void main(String[] args) {
-        userRepository(1)
+        int nextInt = ThreadLocalRandom.current().nextInt(1, 5);
+        userRepository(nextInt)
                 .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
     }
 
@@ -14,6 +17,7 @@ public class MonoEmptyOrError {
      * A Proper Mono using Faker library
      */
     private static Mono<String> userRepository(int userId) {
+        System.out.println("user id: " + userId);
         if (userId == 1) {
             return Mono.just(Util.faker().name().fullName());
         } else if (userId == 2) {
