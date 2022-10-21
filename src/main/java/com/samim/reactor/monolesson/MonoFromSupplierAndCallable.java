378efdb019fd74746cdc08b143b05b36189cec0e
Samim <samimaftabahmed@gmail.com>
2022-10-22 02:33:29 +0530

    Mono using just vs fromSupplier and fromCallable

---
+++ src/main/java/com/samim/reactor/monolesson/MonoFromSupplierAndCallable.java
@@ -0,0 +1,30 @@
+package com.samim.reactor.monolesson;
+
+import com.samim.reactor.courseutil.Util;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
+/**
+ * Behaviour of Mono from Supplier and Callable
+ */
+public class MonoFromSupplierAndCallable {
+
+    public static void main(String[] args) {
+
+//        This mono will call its underlying method even without any subscriber
+        Mono.just(Util.fullName());
+
+//        This mono will call its underlying method only when there is a subscriber
+        Supplier<String> stringSupplier = () -> Util.fullName();
+        Mono<String> fromSupplier = Mono.fromSupplier(stringSupplier);
+//        fromSupplier.subscribe(Util.onNext());
+
+        Callable<String> stringCallable = () -> Util.fullName();
+//        Underlying methods in a Mono from callable and supplier are called lazily
+        Mono<String> fromCallable = Mono.fromCallable(stringCallable);
+//        fromCallable.subscribe(Util.onNext());
+    }
+
+}
