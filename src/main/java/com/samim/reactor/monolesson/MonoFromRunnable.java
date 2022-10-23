6736272c78504384fb7e0f4b34f8ba27d7a7c2b8
Samim <samimaftabahmed@gmail.com>
2022-10-23 14:40:43 +0530

    Mono from Runnable

---
+++ src/main/java/com/samim/reactor/monolesson/MonoFromRunnable.java
@@ -0,0 +1,25 @@
+package com.samim.reactor.monolesson;
+
+import com.samim.reactor.courseutil.Util;
+import reactor.core.publisher.Mono;
+
+public class MonoFromRunnable {
+
+    public static void main(String[] args) {
+        Mono.fromRunnable(timeConsumingProcess())
+                .subscribe(
+                        Util.onNext(),
+                        Util.onError(),
+                        () -> System.out.println("Operation done after time consuming mono completion like sending emails...")
+                );
+    }
+
+    private static Runnable timeConsumingProcess() {
+        return () -> {
+            System.out.println("Operation starting...");
+            Util.sleepSeconds(3);
+            System.out.println("Operation completed...");
+        };
+    }
+
+}
