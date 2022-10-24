eb57308c6983da0c789f4e8ee2d8bcba38087af2
Samim <samimaftabahmed@gmail.com>
2022-10-25 00:02:51 +0530

    refactor

--- src/main/java/com/samim/reactor/lesson/flux/FluxBasics.java
+++ src/main/java/com/samim/reactor/lesson/flux/FluxBasics.java
@@ -9,16 +9,16 @@ import java.util.List;
 public class FluxBasics {
 
     public static void main(String[] args) {
-        System.out.println("-- 1 --");
+        System.out.println("-- Flux.just --");
         Flux.just(1, 2, 3, 4, "abc", Util.fullName())
                 .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
 
-        System.out.println("-- 2 --");
+        System.out.println("-- Flux.fromIterable --");
         List<String> strings = Arrays.asList("a", "b", "c");
         Flux.fromIterable(strings)
                 .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
 
-        System.out.println("-- 3 --");
+        System.out.println("-- Flux.fromArray --");
         Integer[] arr = {1, 2, 8, 6};
         Flux.fromArray(arr)
                 .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
