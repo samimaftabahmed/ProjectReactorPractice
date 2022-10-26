d33bb84c4b396574d4a076ddacca399d06651ed9
Samim <samimaftabahmed@gmail.com>
2022-10-27 00:22:42 +0530

    Flux Range

---
+++ src/main/java/com/samim/reactor/lesson/flux/FluxRange.java
@@ -0,0 +1,32 @@
+package com.samim.reactor.lesson.flux;
+
+import com.samim.reactor.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+public class FluxRange {
+
+    public static void main(String[] args) {
+        System.out.println("---- intRange1() ----");
+        intRange1();
+        System.out.println("\n---- intRange2() ----");
+        intRange2();
+        System.out.println("\n---- stringRange() ----");
+        stringRange();
+    }
+
+    private static void intRange1() {
+        Flux.range(0, 12)
+                .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
+    }
+
+    private static void intRange2() {
+        Flux.range(3, 12)
+                .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
+    }
+
+    private static void stringRange() {
+        Flux.range(3, 12)
+                .map(integer -> Util.fullName())
+                .subscribe(Util.onNext(), Util.onError(), Util.onComplete());
+    }
+}
