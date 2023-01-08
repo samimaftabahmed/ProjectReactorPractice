c7e379789b921589bf37e3c693125ef226a6570a
Samim <samimaftabahmed@gmail.com>
2023-01-08 20:49:38 +0530

    Flux create refacting

--- src/main/java/com/samim/reactor/lesson/flux/FluxCreateFluxSink.java
+++ src/main/java/com/samim/reactor/lesson/flux/FluxCreateFluxSink.java
@@ -4,7 +4,7 @@ import com.samim.reactor.courseutil.NameProducer;
 import com.samim.reactor.courseutil.Util;
 import reactor.core.publisher.Flux;
 
-public class FluxCreate {
+public class FluxCreateFluxSink {
 
     public static void main(String[] args) {
 
@@ -15,6 +15,39 @@ public class FluxCreate {
         boundedEmitting();
 
         System.out.println("\n\n-- Create Flux with Consumer FluxSink");
+        emittingFromThreads();
+
+    }
+
+    private static void unBoundedEmitting() {
+        Flux.create(fluxSink -> {
+                    String country;
+                    do {
+                        country = Util.faker().country().name();
+                        System.out.println("emitting : " + country);
+                        fluxSink.next(country);
+                    } while (!country.equalsIgnoreCase("canada"));
+//                    fluxSink.complete();
+                })
+                .take(5) // take method calls the onComplete method at last
+                .subscribe(Util.subscriber());
+    }
+
+    private static void boundedEmitting() {
+        Flux.create(fluxSink -> {
+                    String country;
+                    do {
+                        country = Util.faker().country().name();
+                        System.out.println("emitting : " + country);
+                        fluxSink.next(country);
+                    } while (!country.equalsIgnoreCase("canada") && !fluxSink.isCancelled());
+//                    fluxSink.complete(); // redundant since take() method is called
+                })
+                .take(5)
+                .subscribe(Util.subscriber());
+    }
+
+    public static void emittingFromThreads() {
         NameProducer nameProducer = new NameProducer();
         Flux.create(nameProducer).subscribe(Util.subscriber());
         nameProducer.produce();
@@ -26,34 +59,5 @@ public class FluxCreate {
         for (int i = 0; i < 10; i++) {
             new Thread(runnable).start();
         }
-
-    }
-
-    private static void unBoundedEmitting(){
-        Flux.create(fluxSink -> {
-                    String country;
-                    do {
-                        country = Util.faker().country().name();
-                        System.out.println("emitting : " + country);
-                        fluxSink.next(country);
-                    } while (!country.equalsIgnoreCase("canada"));
-                    fluxSink.complete();
-                })
-                .take(5)
-                .subscribe(Util.subscriber());
-    }
-
-    private static void boundedEmitting(){
-        Flux.create(fluxSink -> {
-                    String country;
-                    do {
-                        country = Util.faker().country().name();
-                        System.out.println("emitting : " + country);
-                        fluxSink.next(country);
-                    } while (!country.equalsIgnoreCase("canada") && !fluxSink.isCancelled());
-                    fluxSink.complete();
-                })
-                .take(5)
-                .subscribe(Util.subscriber());
     }
 }
